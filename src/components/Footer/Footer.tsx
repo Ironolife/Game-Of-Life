d618790a3f382df9a56129c66ea8116e886702d8
Ironolife <40685147+Ironolife@users.noreply.github.com>
2021-03-08 17:26:48 +0800

    Fixed Footer link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className='relative p-4 flex-shrink-0 text-center bg-black z-10'>
-    <a href='https://github.com/' target='_black' rel='noreferrer'>
+    <a href='https://github.com/Ironolife' target='_black' rel='noreferrer'>
       Made with <span className='text-pink-500'>♥</span> by Ironolife © 2021
     </a>
   </footer>
